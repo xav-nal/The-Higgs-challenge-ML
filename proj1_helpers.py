9f8c285fc61427664007d266886c692594c777e0
set-up initial files <xavier.nal@epfl.ch>
2021-10-29 01:25:45 +0200

    First Prototype 0,62

--- proj1_helpers.py
+++ proj1_helpers.py
@@ -30,6 +30,7 @@ def load_csv_data(data_path, sub_sample=False):
 def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data matrix"""
     y_pred = np.dot(data, weights)
+    print(y_pred)
     y_pred[np.where(y_pred <= 0)] = -1
     y_pred[np.where(y_pred > 0)] = 1
     
@@ -48,4 +49,4 @@ def create_csv_submission(ids, y_pred, name):
         writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
-            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+            writer.writerow({'Id':int(r1),'Prediction':r2})
